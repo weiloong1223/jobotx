10056a688c8fe9464b8a549daff5a765264ae7fa
weiloong1223 <weiloong614@gmail.com>
2025-03-13 17:38:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-flask
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello from IBM Cloud Code Engine!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
